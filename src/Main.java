c952428f079149baf211762c5b5dcc40961a18ed
Cl1yMba <petrmanastryly199@gmail.com>
2023-09-18 19:22:55 +0300

    HomeWork 10 Done!

--- src/Main.java
+++ src/Main.java
@@ -3,68 +3,43 @@ public class Main {
         task1();
         task2();
         task3();
-        task4();
+
 
     }
 
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-        }
-        return arr;
-    }
 
     public static void task1() {
-        int[] arr = generateRandomArray();
-        int summTillMonth = 0;
-
-        for (int current : arr) {
-            summTillMonth += current;
-        }
-        System.out.println("Сумма трат за месяц составила " + summTillMonth + " рублей");
+       String firstName = "Ivan";
+       String middleName = "Ivanovich";
+       String lastName = "Ivanov";
+       String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
     }
 
     public static void task2() {
-        int[] arr = generateRandomArray();
-        int minForOneDay = arr[0];
-        int maxForOneDay = arr[0];
-
-        for (int current : arr) {
-            if (current > maxForOneDay) {
-                maxForOneDay = current;
-            }
-            if (current < minForOneDay) {
-                minForOneDay = current;
-            }
-        }
-        System.out.println("Минимальная сумма трат за день составила " + minForOneDay + " рублей. Максимальная сумма трат за день составила " + maxForOneDay + " рублей");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String fullNameInUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameInUpperCase);
 
     }
-
 
     public static void task3() {
-        int[] arr = generateRandomArray();
-        double allExpensses = 0;
-        int daysInMonth = 30;
-        for (int current : arr) {
-            allExpensses += current;
-        }
-        allExpensses /= daysInMonth;
-
-        System.out.println("Средняя сумма трат за месяц составила " + allExpensses + " рублей");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String fullNameCorrectForProgram = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameCorrectForProgram);
 
     }
 
-    public static void task4() {
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-        }
+
+
 
 
 
     }
-}
